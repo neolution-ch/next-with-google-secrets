cfb3e9afbd2c62d5f8fbd8f75a1ec4fe55cbc0fd
neotob <tobias.merki@neolution.ch>
2023-09-14 12:01:30 +0200

    changed name of new config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,7 +14,7 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: "eslint-config.json",
+    project: "tsconfig.eslint.json",
     ecmaVersion: 2018,
     sourceType: "module",
   },
@@ -26,7 +26,7 @@ module.exports = {
     "import/resolver": {
       typescript: {
         alwaysTryTypes: true,
-        project: ["eslint-config.json"],
+        project: ["tsconfig.eslint.json"],
       },
       node: {
         extensions: [".js", ".jsx", ".ts", ".tsx"],
